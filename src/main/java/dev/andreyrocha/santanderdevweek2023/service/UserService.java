a74fc284c650b376786d8d41a7212eca8f94afd7
AR <ahrocha@gmail.com>
2023-10-22 15:01:41 -0300

    Endpoints ajustados

--- src/main/java/dev/andreyrocha/santanderdevweek2023/service/UserService.java
+++ src/main/java/dev/andreyrocha/santanderdevweek2023/service/UserService.java
@@ -1,10 +1,33 @@
 package dev.andreyrocha.santanderdevweek2023.service;
 
 import dev.andreyrocha.santanderdevweek2023.domain.model.User;
+import dev.andreyrocha.santanderdevweek2023.domain.repository.UserRepository;
+import dev.andreyrocha.santanderdevweek2023.service.UserService;
+import dev.andreyrocha.santanderdevweek2023.service.impl.UserServiceImpl;
 
-public interface UserService {
+import org.springframework.stereotype.Service;
 
-    User findById(Long id);
+import java.util.NoSuchElementException;
 
-    User create(User userToCreate);
+@Service
+public class UserService implements UserServiceImpl {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
 }
